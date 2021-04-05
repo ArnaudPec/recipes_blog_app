6eaaf7c7612047d0d8d001837b932608b64e001a
Arnaud Pecoraro <arnaud.pecoraro@outlook.com>
2021-04-05 12:14:21 +0200

    part 2

---
+++ flask_blog.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, url_for
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey S',
+        'title': 'Post 1',
+        'content': 'content1',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Post 2',
+        'content': 'content2',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
